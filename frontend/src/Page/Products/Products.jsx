0d14ff9ad6496350088e222bd435fae245c9a268
mohitsvpi <singhalmohit011@gmail.com>
2023-07-26 22:41:56 +0530

    Product page started

--- frontend/src/Page/Products/Products.jsx
+++ frontend/src/Page/Products/Products.jsx
@@ -1,9 +1,25 @@
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, Select, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, Image, Select, SimpleGrid, Text } from '@chakra-ui/react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { MdArrowForwardIos } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
 const Products = () => {
+  const [products, setProducts] = useState();
+
+  const fetchProducts = () => {
+    axios.get("https://dummyjson.com/products")
+    .then((res) => {
+      console.log(res)
+      setProducts(res.data.products)
+    })
+  }
+
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
     <Box w="90%" m="auto">
       <Box>
@@ -18,11 +34,11 @@ const Products = () => {
         </Box>
         <Flex>
           <Box w="30%">
-            
+
           </Box>
           <Box w="70%">
               <Flex justifyContent={'space-between'}>
-                <Text>Showing 9 results from total 37 </Text>
+                <Text>Showing 9 results from total {products && products.length}</Text>
                 <Flex w="30%" justifyContent={'space-evenly'} alignItems={'center'} gap={2}>
                   <Text w="40%">Sort by</Text>
                   <Select>
@@ -32,6 +48,28 @@ const Products = () => {
                   </Select>
                 </Flex>
               </Flex>
+              <SimpleGrid columns={3} gap={10} m="50px 0">
+                  {
+                    products && products.map((product, index) => (
+                      <Link to={`/products/${product.id}`} key={product.id}>
+                        <Box  boxShadow={'md'} p={5} borderRadius={'20px'} h="360px">
+                          <Image src={product.images[0]} h="250px" w="full"/>
+                          <Box m="20px 0">
+                            <Flex justifyContent={'space-between'}>
+                              <Box>
+                                <Text>{product.title}</Text>
+                              </Box>
+                              <Box>
+                                <Text as="b">₹{product.price}</Text>
+                              </Box>
+                            </Flex>
+                            <Flex></Flex>
+                          </Box>
+                        </Box>
+                      </Link>
+                    ))
+                  }
+              </SimpleGrid>
           </Box>
         </Flex>
     </Box>
